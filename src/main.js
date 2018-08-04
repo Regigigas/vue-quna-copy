03f8d992da02428cb30f32221facca111705b4dc
tomli <lishuminvip@163.com>
2018-08-04 21:27:33 +0800

    重新部署环境

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import router from './router'
+
+import Fastclick from 'fastclick'
 
 Vue.config.productionTip = false
-
+Fastclick.attach(document.body)
+/* eslint-disable no-new */
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
